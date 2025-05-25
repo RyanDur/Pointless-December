3149e7e59ee48c91184092cac0d90ab97f3611aa
Ryan Durling <ryan.p.durling@gmail.com>
2025-05-25 16:02:17 -0500

    test stream unary

--- test_greet.py
+++ test_greet.py
@@ -24,3 +24,18 @@ def test_greet_unary_stream():
         hello_replies = client.ParrotSayHello(hello_request)
         replies = [reply.retort for reply in hello_replies]
         assert replies == ["Server says: Chirp Chirp! 0", "Server says: Chirp Chirp! 1", "Server says: Chirp Chirp! 2"]
+
+def test_greet_stream_unary():
+    with grpc.insecure_channel('localhost:50051') as channel:
+        client = greet_pb2_grpc.GreeterStub(channel)
+
+        def get_client_stream_function():
+            for name in ["Alice", "Bob", "Charlie"]:
+                yield greet_pb2.HelloRequest(salutation=name)
+
+        delayed_reply = client.ChattyClientSaysHello(get_client_stream_function())
+        assert delayed_reply.message == "You have sent 3 messages. Please expect a delayed response."
+        assert len(delayed_reply.request) == 3
+        assert delayed_reply.request[0].salutation == "Alice"
+        assert delayed_reply.request[1].salutation == "Bob"
+        assert delayed_reply.request[2].salutation == "Charlie"
