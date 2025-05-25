5bc3d9c2a93dba7691ae9a7aa30f6acc8d6e1dc3
Ryan Durling <ryan.p.durling@gmail.com>
2025-05-25 15:54:22 -0500

    test unary stream

--- test_greet.py
+++ test_greet.py
@@ -14,3 +14,11 @@ async def test_greet_unary_unary():
         hello_request = greet_pb2.HelloRequest(salutation="Bonjour")
         hello_reply = client.SayHello(hello_request)
         assert hello_reply.retort == "Hola!"
+
+def test_greet_unary_stream():
+    with grpc.insecure_channel('localhost:50051') as channel:
+        client = greet_pb2_grpc.GreeterStub(channel)
+        hello_request = greet_pb2.HelloRequest(salutation="Bonjour")
+        hello_replies = client.ParrotSayHello(hello_request)
+        replies = [reply.retort for reply in hello_replies]
+        assert replies == ["Server says: Chirp Chirp! 0", "Server says: Chirp Chirp! 1", "Server says: Chirp Chirp! 2"]
