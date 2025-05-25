91551990c248b84417c50c53c5380e0c2ead0490
Ryan Durling <ryan.p.durling@gmail.com>
2025-05-25 16:08:36 -0500

    extract common client

--- test_greet.py
+++ test_greet.py
@@ -1,6 +1,3 @@
-from concurrent import futures
-from unittest.mock import patch
-
 import grpc
 import pytest
 
@@ -8,50 +5,49 @@ import greet_pb2
 import greet_pb2_grpc
 
 
+@pytest.fixture()
+def client():
+    with grpc.insecure_channel('localhost:50051') as channel:
+        yield greet_pb2_grpc.GreeterStub(channel)
+
+
 @pytest.mark.asyncio
-async def test_greet_unary_unary():
-    with grpc.insecure_channel('localhost:50051') as channel:
-        client = greet_pb2_grpc.GreeterStub(channel)
-        hello_request = greet_pb2.HelloRequest(salutation="Bonjour")
-        hello_reply = client.SayHello(hello_request)
-        assert hello_reply.retort == "Hola!"
+async def test_greet_unary_unary(client):
+    hello_request = greet_pb2.HelloRequest(salutation="Bonjour")
+    hello_reply = client.SayHello(hello_request)
+    assert hello_reply.retort == "Hola!"
 
-def test_greet_unary_stream():
-    with grpc.insecure_channel('localhost:50051') as channel:
-        client = greet_pb2_grpc.GreeterStub(channel)
-        hello_request = greet_pb2.HelloRequest(salutation="Bonjour")
-        hello_replies = client.ParrotSayHello(hello_request)
-        replies = [reply.retort for reply in hello_replies]
-        assert replies == ["Server says: Chirp Chirp! 0", "Server says: Chirp Chirp! 1", "Server says: Chirp Chirp! 2"]
 
-def test_greet_stream_unary():
-    with grpc.insecure_channel('localhost:50051') as channel:
-        client = greet_pb2_grpc.GreeterStub(channel)
+def test_greet_unary_stream(client):
+    hello_request = greet_pb2.HelloRequest(salutation="Bonjour")
+    hello_replies = client.ParrotSayHello(hello_request)
+    replies = [reply.retort for reply in hello_replies]
+    assert replies == ["Server says: Chirp Chirp! 0", "Server says: Chirp Chirp! 1", "Server says: Chirp Chirp! 2"]
 
-        def get_client_stream_function():
-            for name in ["Alice", "Bob", "Charlie"]:
-                yield greet_pb2.HelloRequest(salutation=name)
 
-        delayed_reply = client.ChattyClientSaysHello(get_client_stream_function())
-        assert delayed_reply.message == "You have sent 3 messages. Please expect a delayed response."
-        assert len(delayed_reply.request) == 3
-        assert delayed_reply.request[0].salutation == "Alice"
-        assert delayed_reply.request[1].salutation == "Bob"
-        assert delayed_reply.request[2].salutation == "Charlie"
+def test_greet_stream_unary(client):
+    def get_client_stream_function():
+        for name in ["Alice", "Bob", "Charlie"]:
+            yield greet_pb2.HelloRequest(salutation=name)
 
-def test_greet_stream_stream():
-    with grpc.insecure_channel('localhost:50051') as channel:
-        client = greet_pb2_grpc.GreeterStub(channel)
+    delayed_reply = client.ChattyClientSaysHello(get_client_stream_function())
+    assert delayed_reply.message == "You have sent 3 messages. Please expect a delayed response."
+    assert len(delayed_reply.request) == 3
+    assert delayed_reply.request[0].salutation == "Alice"
+    assert delayed_reply.request[1].salutation == "Bob"
+    assert delayed_reply.request[2].salutation == "Charlie"
 
-        def get_client_stream_function():
-            for name in ["Alice", "Bob", "Charlie"]:
-                yield greet_pb2.HelloRequest(salutation=name)
 
-        responses = client.InteractingHello(get_client_stream_function())
-        expected_responses = [
-            "Hello Alice, how are you?",
-            "Hello Bob, how are you?",
-            "Hello Charlie, how are you?"
-        ]
-        for response, expected in zip(responses, expected_responses):
-            assert response.retort == expected
+def test_greet_stream_stream(client):
+    def get_client_stream_function():
+        for name in ["Alice", "Bob", "Charlie"]:
+            yield greet_pb2.HelloRequest(salutation=name)
+
+    responses = client.InteractingHello(get_client_stream_function())
+    expected_responses = [
+        "Hello Alice, how are you?",
+        "Hello Bob, how are you?",
+        "Hello Charlie, how are you?"
+    ]
+    for response, expected in zip(responses, expected_responses):
+        assert response.retort == expected
